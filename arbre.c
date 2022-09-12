9468661142e4bd5bd3dbd394aa5def627bd83eea
unknown <zaaferkenza08@gmail.com>
2022-09-12 14:04:19 +0100

    done

--- arbre.c
+++ arbre.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "functions.h"
 void main(){
-   int num,i;
+   int num,i,T;
     printf("Entrer la taille de votre arbre : ");
+    scanf("%d",&T);
+    printf("Entrer la taille du premier triangle : ");
     scanf("%d",&num);
-    for(i=0;i<3;i++){
+    for(i=0;i<T;i++){
       triangle(num+i,num-i); 
     }
 }
